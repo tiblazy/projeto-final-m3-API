b61de226fff0ca269587c7ed88cc01b2ebfdad09
tiblazy <hauntedapply@gmail.com>
2022-07-05 07:31:24 -0300

    init: README

--- server.js
+++ server.js
@@ -11,7 +11,7 @@ app.db = router.db;
 const rules = auth.rewriter({
   users: 640,
   tables: 664,
-  role: 440
+  role: 440,
 });
 
 app.use(cors());
@@ -19,4 +19,3 @@ app.use(rules);
 app.use(auth);
 app.use(router);
 app.listen(port);
-
