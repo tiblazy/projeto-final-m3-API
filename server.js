820539a7faa45aaf9256132763b41c1e690b7e5b
tiblazy <hauntedapply@gmail.com>
2022-07-05 06:46:35 -0300

    init: projeto final API

---
+++ server.js
@@ -0,0 +1,26 @@
+const jsonServer = require("json-server");
+const auth = require("json-server-auth");
+const cors = require("cors");
+const port = process.env.PORT || 3001;
+
+const app = jsonServer.create();
+const router = jsonServer.router("db.json");
+
+app.db = router.db;
+
+const rules = auth.rewriter({
+  users: 640,
+  tables: 664,
+  role: 440,
+
+  listUsers: "/users",
+  listTables: "/tables",
+  myTables: "/users?tables_like=:id",
+});
+
+app.use(cors());
+app.use(rules);
+app.use(auth);
+app.use(router);
+app.listen(port);
+
